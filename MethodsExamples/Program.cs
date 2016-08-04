b0143bec0000e7b0617889784630f7e513cb83c3
Heber <heberdanielorellana@gmail.com>
2016-08-04 11:49:26 -0600

    ..

--- MethodsExamples/Program.cs
+++ MethodsExamples/Program.cs
@@ -11,10 +11,10 @@ namespace MethodsExamples
         static void SumatoriaPareseImpares()
         {
             int SumaPares = 0, SumaImpares=0;
-            for (int i = 10; i< 125; i++)
-            {
+            //for (int i = 10; i< 125; i++)
+            //{
 
-            }
+            //}
         }
         static bool EsPar(int Numero)
         {
@@ -26,10 +26,38 @@ namespace MethodsExamples
             else { Bandera = false; }
             return Bandera;
         }
+        static bool EsPar(string Numero, out int NumeroEntero)
+        {
+            int num = 0; bool Flag = false;
+            try
+            {
+                num = int.Parse(Numero);
+                Flag = ((num % 2) == 0);
+
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Solo puede ingresar números. {0}", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: ", ex.Message);
+            }
+            finally
+            {
+                NumeroEntero = num;
+            }
+            return Flag;
+
+        }
         static void Main(string[] args)
         {
+            string sNum = ""; int Num = 0;
+            Console.WriteLine("Ingrese numero:");
+            sNum = Console.ReadLine();
 
-            if (EsPar(2))
+            if (EsPar(sNum, out Num))
             {
                 Console.WriteLine("El numero es par.");
             }
